65ab28d3bb396274149a6334ce4684a149321c58
mkoteswararao13 <Padma@9848>
2018-06-21 00:09:29 -0500

    ggg

---
+++ WEBCORE/Pages/EMP.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WEBCORE.Pages
+{
+    public class EMPModel : PageModel
+    {
+        [BindProperty(SupportsGet =true)]
+        public string fname { get; set; }
+        public IActionResult OnGetAsync()
+
+        {
+            fname = "koti";
+            return Page();
+
+        }
+        public IActionResult OnPostAsync()
+
+        {
+            fname = "koti";
+            return Page();
+
+        }
+
+        public IActionResult OnPostDeleteAsync()
+
+        {
+            fname = "koti";
+            return Page();
+
+        }
+    }
+}
